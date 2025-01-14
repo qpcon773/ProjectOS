513ca6af89abbbda4c1c3ecd51cf99827e9cd6fa
LAPTOP-I411U8ME\User <william@yunin.info>
2025-01-14 23:09:50 +0800

    !! Edit !! 選單初版 & 首頁元件

--- src/components/NavComponent.vue
+++ src/components/NavComponent.vue
@@ -32,11 +32,15 @@ const rwdSwitch = ref(false)
 </script>
 
 <template>
-  <nav>
+  <nav :class="{focus: rwdSwitch}">
     <a href="javascript:;" class="logo"
       ><img src="@/assets/images/logo.svg" alt=""
     /></a>
-    <a href="javascript:;" class="hamBtn" :class="{focus: rwdSwitch}" @click="rwdSwitch = !rwdSwitch"></a>
+    <a href="javascript:;" class="hamBtn" @click="rwdSwitch = !rwdSwitch">
+      <b></b>
+      <b></b>
+      <b></b>
+    </a>
     <ul class="navList">
       <template v-for="(data, id) in navData" :key="id">
         <li>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,5 +1,42 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
+  <main>
+    <section class="eventArea">
+      <div class="eventListBox">
+        <ul>
+          <template v-for="(data, id) in 3" :key="id">
+            <li>
+              <a href="javascript:;">
+                <div class="imgBox">
+                  <img src="https://picsum.photos/300" alt="" />
+                </div>
+                <div class="infoBox">
+                  <p>標題標題標題標題標題標題{{ data }}</p>
+                  <span>文字文字文字文字文字文字文字文字</span>
+                </div>
+              </a>
+            </li>
+          </template>
+        </ul>
+        <ul>
+          <template v-for="(data, id) in 3" :key="id">
+            <li>
+              <a href="javascript:;">
+                <div class="imgBox">
+                  <img src="https://picsum.photos/300" alt="" />
+                </div>
+                <div class="infoBox">
+                  <p>標題標題標題標題標題標題{{ id }}</p>
+                  <span>文字文字文字文字文字文字文字文字</span>
+                </div>
+              </a>
+            </li>
+          </template>
+        </ul>
+      </div>
+    </section>
+  </main>
 </template>
+
+<style lang="scss" src="@/assets/scss/views/index.scss" scoped></style>
