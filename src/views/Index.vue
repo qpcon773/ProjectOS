b192bed7946a01db2df03260e3602ae9facc5c7e
LAPTOP-I411U8ME\User <william@yunin.info>
2025-01-26 16:21:29 +0800

    !! Edit !! swiper套件

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,5 +1,9 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
+
+import { Swiper, SwiperSlide, useSwiper } from "swiper/vue";
+import "swiper/css";
+
 import UseSvg from "@/components/UseSvg.vue";
 
 const polluteImg = ref([
@@ -10,8 +14,47 @@ const polluteImg = ref([
   new URL("@/assets/images/index/pollute_05.jpg", import.meta.url).href,
 ]);
 
-onMounted(() => {
-});
+const swiperItem = ref();
+
+const actionData = ref([
+  {
+    date: "1/1",
+    title: "Test01",
+    text: "Test Test Test Test Test Test Test Test Test Test",
+    btnText: "Test",
+  },
+  {
+    date: "5/1",
+    title: "Test02",
+    text: "Test2 Test2 Test2 Test2 Test2 Test2 Test2 Test2 Test2 Test2",
+    btnText: "Test2",
+  },
+  {
+    date: "8/1",
+    title: "Test03",
+    text: "Test3 Test3 Test3 Test3 Test3 Test3 Test3 Test3 Test3 Test3",
+    btnText: "Test3",
+  },
+  {
+    date: "12/1",
+    title: "Test04",
+    text: "Test4 Test4 Test4 Test4 Test4 Test4 Test4 Test4 Test4 Test4",
+    btnText: "Test4",
+  },
+]);
+
+onMounted(() => {});
+
+const onSwiper = (swiper) => {
+  swiperItem.value = swiper;
+  console.log(swiper)
+};
+
+const slideEvent = (e) => {
+  swiperItem.value.slideTo(e);
+};
+
+const swiperIndex = computed(()=> swiperItem.value?.activeIndex)
 </script>
 
 <template>
@@ -39,7 +82,7 @@ onMounted(() => {
                 <p class="title">
                   標題標題標題標題標題標題標題標題標題標題標題標題標題標題標題標題標題標題標題標題標題標題標題標題標題標題標題標題
                 </p>
-                <div class="iconBox">
+                <div class="fishBox">
                   <UseSvg name="fish" />
                   <UseSvg name="fish" />
                 </div>
@@ -56,7 +99,6 @@ onMounted(() => {
           <img :src="data" alt="" />
         </div>
       </template>
-
       <ul class="pollutePhotoList">
         <li></li>
         <li></li>
@@ -64,7 +106,45 @@ onMounted(() => {
       </ul>
     </section>
 
-    <section class="actionArea mainBlock"></section>
+    <section class="actionArea mainBlock">
+      <ul>
+        <li :class="{focus: swiperIndex === 0}"><a href="javascript:;" @click="slideEvent(0)">內容0</a></li>
+        <li :class="{focus: swiperIndex === 1}"><a href="javascript:;" @click="slideEvent(1)">內容1</a></li>
+        <li :class="{focus: swiperIndex === 2}"><a href="javascript:;" @click="slideEvent(2)">內容2</a></li>
+        <li :class="{focus: swiperIndex === 3}"><a href="javascript:;" @click="slideEvent(3)">內容3</a></li>
+      </ul>
+      <swiper
+        :slidesPerView="'auto'"
+        :spaceBetween="24"
+        :loop="true"
+        @swiper="onSwiper"
+      >
+        <template v-for="(data, id) in actionData" :key="id">
+          <swiper-slide>
+            <div class="actionBox">
+              <div class="imgBox">
+                <p class="date">{{ data.date }}</p>
+                <img src="https://picsum.photos/400/300" alt="" />
+              </div>
+              <div class="infoBox">
+                <p class="title">{{ data.title }}</p>
+                <p class="text">{{ data.text }}</p>
+                <div class="btnBox">
+                  <a href="javascript:;" class="btn">
+                    <p>{{ data.btnText }}</p>
+
+                    <div class="fishBox">
+                      <UseSvg name="fish" />
+                      <UseSvg name="fish" />
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </swiper-slide>
+        </template>
+      </swiper>
+    </section>
 
     <section class="shopArea mainBlock">
       <div class="shopListBox">
@@ -91,13 +171,23 @@ onMounted(() => {
                   <img src="https://picsum.photos/300" alt="" />
                 </div>
                 <div class="infoBox">
-                  <p>標題標題標題標題標題標題{{ id }}</p>
+                  <p>標題標題標題標題標題標題</p>
                   <span>文字文字文字文字文字文字文字文字</span>
                 </div>
               </a>
             </li>
           </template>
         </ul>
+      </div>
+      <div class="btnBox">
+        <a href="javascript:;" class="btn">
+          <p>查看更多</p>
+
+          <div class="fishBox">
+            <UseSvg name="fish" />
+            <UseSvg name="fish" />
+          </div>
+        </a>
       </div>
     </section>
   </main>
